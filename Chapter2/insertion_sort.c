938b90a2be0d54fd1cfed6a37db1464041206723
grejioh <grejioh@outlook.com>
2021-03-17 22:41:06 +0800

    fix a bug in insertion_sort.c

--- Chapter2/insertion_sort.c
+++ Chapter2/insertion_sort.c
@@ -1,24 +1,23 @@
-#include<stdio.h>
-int insertion_sort(int *a, int n){
-	for(int j=2; j<=n; j++){
-		int key = a[j];
-		int i = j-1;
-		while(i>0 && a[i]>key)
-		{
-			a[i+1] = a[i];
-			i--;
-		}
-		a[i+1] = key;
-	}
+#include <stdio.h>
+void insertion_sort(int *a, int n) {
+  for (int j = 2; j <= n; j++) {
+    int key = a[j];
+    int i = j - 1;
+    while (i > 0 && a[i] > key) {
+      a[i + 1] = a[i];
+      i--;
+    }
+    a[i + 1] = key;
+  }
 }
-int main(){
-	int n, a[1000];
-	scanf("%d", &n);
-	for(int i=1; i<=n; i++) {
-		scanf("%d", &a[i]);
-	}
-	insertion_sort(a, n);
-	for(int i=1; i<=n; i++)
-		printf("%d ", a[i]);
-	return 0;
+int main() {
+  int n, a[1000];
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    scanf("%d", &a[i]);
+  }
+  insertion_sort(a, n);
+  for (int i = 1; i <= n; i++)
+    printf("%d ", a[i]);
+  return 0;
 }
